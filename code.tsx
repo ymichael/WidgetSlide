66928f299d16aa8c0de173b7bc174f96ad29ad84
Michael Yong <wrong92@gmail.com>
2021-10-29 12:24:48 -0400

    use image

--- code.tsx
+++ code.tsx
@@ -21,8 +21,9 @@ function EmptyTile() {
 
 function SlideTile({ row, col }: { row: number; col: number }) {
   return (
-    <AutoLayout stroke="#123456" cornerRadius={10}>
+    <AutoLayout stroke="#123456" strokeWidth={1} cornerRadius={10}>
       <Frame width={TILE_SIZE} height={TILE_SIZE}>
+        <Frame width={IMAGE_SIZE} height={IMAGE_SIZE} fill="#FDF6EB" />
         <Image
           x={-TILE_SIZE * col - TILE_GAP * col}
           y={-TILE_SIZE * row - TILE_GAP * row}
@@ -83,11 +84,13 @@ const shuffledSolvableInitialStates = [
   [4, 1, 7, 0, 6, 5, 3, 8, 2],
 ];
 
+const randomInitialState =
+  shuffledSolvableInitialStates[
+    Math.floor(shuffledSolvableInitialStates.length * Math.random())
+  ];
+
 function SlidePuzzle() {
-  const [tiles, setTiles] = useSyncedState(
-    "puzzle",
-    shuffledSolvableInitialStates[0]
-  );
+  const [tiles, setTiles] = useSyncedState("puzzle", randomInitialState);
 
   const emptyTileIdx = tiles.indexOf(8);
   const [emptyRow, emptyCol] = tileIdxToCoords(emptyTileIdx);
@@ -95,114 +98,71 @@ function SlidePuzzle() {
   return (
     <AutoLayout
       direction="vertical"
-      cornerRadius={15}
-      fill="#A66300"
-      effect={[
-        {
-          type: "drop-shadow",
-          color: {
-            r: 0.25833332538604736,
-            g: 0.1549999713897705,
-            b: 0,
-            a: 0.9200000166893005,
-          },
-          offset: { x: 2, y: 2 },
-          spread: 0,
-          visible: true,
-          blendMode: "normal",
-          showShadowBehindNode: false,
-          blur: 1,
-        },
-      ]}
       width={388}
       height={457}
-      padding={10}
       horizontalAlignItems="center"
     >
-      <AutoLayout
-        width="fill-parent"
-        height={80}
-        horizontalAlignItems="center"
-        verticalAlignItems="center"
-      >
-        <Text
-          fill="#A66300"
-          effect={[
-            {
-              type: "inner-shadow",
-              color: {
-                r: 0.44999998807907104,
-                g: 0.2699999511241913,
-                b: 0,
-                a: 1,
-              },
-              offset: { x: 4, y: 4 },
-              spread: 0,
-              visible: true,
-              blendMode: "normal",
-              blur: 10,
-            },
-          ]}
-          fontSize={50}
-          fontFamily="Caveat"
-          fontWeight="bold"
+      <Frame width={388} height={457} cornerRadius={15} fill="#A66300">
+        <Frame
+          y={71}
+          x={19}
+          fill="#955C06"
+          cornerRadius={15}
+          width={350}
+          height={350}
+        />
+        <Frame
+          width={388}
+          height={457}
+          fill={{
+            type: "image",
+            src: "https://static.figma.com/uploads/db163379fd786cb127d6bfd3e0a77fe422721147",
+          }}
+        />
+        <AutoLayout
+          x={25}
+          y={75}
+          direction="vertical"
+          cornerRadius={15}
+          spacing={10}
+          padding={10}
         >
-          Slide
-        </Text>
-      </AutoLayout>
-      <AutoLayout
-        direction="vertical"
-        fill="#955C06"
-        effect={[
-          {
-            type: "inner-shadow",
-            color: { r: 0.5083333253860474, g: 0.3049999475479126, b: 0, a: 1 },
-            offset: { x: 2, y: 2 },
-            spread: 0,
-            visible: true,
-            blendMode: "normal",
-            blur: 1,
-          },
-        ]}
-        cornerRadius={15}
-        spacing={10}
-        padding={10}
-      >
-        {[0, 1, 2].map((row) => {
-          return (
-            <AutoLayout direction="horizontal" spacing={10} key={row}>
-              {[0, 1, 2].map((col) => {
-                const idx = coordsToIdx(row, col);
-                const remappedIdx = tiles[idx];
-                if (remappedIdx === 8) {
-                  return <EmptyTile key={remappedIdx} />;
-                }
-                const [x, y] = tileIdxToCoords(remappedIdx);
-                const isMovable =
-                  (col === emptyCol &&
-                    (row === emptyRow - 1 || row === emptyRow + 1)) ||
-                  (row === emptyRow &&
-                    (col === emptyCol - 1 || col === emptyCol + 1));
-                const additionalProps = isMovable
-                  ? {
-                      onClick: () => {
-                        const newTiles = [...tiles];
-                        newTiles[emptyTileIdx] = remappedIdx;
-                        newTiles[idx] = 8;
-                        setTiles(newTiles);
-                      },
-                    }
-                  : {};
-                return (
-                  <AutoLayout key={remappedIdx} {...additionalProps}>
-                    <SlideTile row={x} col={y} />
-                  </AutoLayout>
-                );
-              })}
-            </AutoLayout>
-          );
-        })}
-      </AutoLayout>
+          {[0, 1, 2].map((row) => {
+            return (
+              <AutoLayout direction="horizontal" spacing={10} key={row}>
+                {[0, 1, 2].map((col) => {
+                  const idx = coordsToIdx(row, col);
+                  const remappedIdx = tiles[idx];
+                  if (remappedIdx === 8) {
+                    return <EmptyTile key={remappedIdx} />;
+                  }
+                  const [x, y] = tileIdxToCoords(remappedIdx);
+                  const isMovable =
+                    (col === emptyCol &&
+                      (row === emptyRow - 1 || row === emptyRow + 1)) ||
+                    (row === emptyRow &&
+                      (col === emptyCol - 1 || col === emptyCol + 1));
+                  const additionalProps = isMovable
+                    ? {
+                        onClick: () => {
+                          const newTiles = [...tiles];
+                          newTiles[emptyTileIdx] = remappedIdx;
+                          newTiles[idx] = 8;
+                          setTiles(newTiles);
+                        },
+                      }
+                    : {};
+                  return (
+                    <AutoLayout key={remappedIdx} {...additionalProps}>
+                      <SlideTile row={x} col={y} />
+                    </AutoLayout>
+                  );
+                })}
+              </AutoLayout>
+            );
+          })}
+        </AutoLayout>
+      </Frame>
     </AutoLayout>
   );
 }
